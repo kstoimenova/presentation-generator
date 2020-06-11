e02813077fd6bdcb6f8044c16ed51cd12ded773f
Katrin Stoimenova <katrinns123@gmail.com>
2020-06-11 14:14:21 +0300

    Add validation when file with the same name already exists 🍻

--- utils/import.php
+++ utils/import.php
@@ -1,3 +1,12 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/import.css">
+    <title>Грешка..</title>
+</head>
+<body>
 <?php
 
 include '../dao/PresentationDao.php';
@@ -20,30 +29,47 @@ if($_POST){
     
     $uploadedImagePath = uploadFile($valid, $errors);
 
-    $presentationDao = new PresentationDao();
-    $presentationId = $presentationDao->save($heading, $categoryId, $uploadedImagePath);   
-
-    $slideTypeDao = new SlideTypeDao();
-    $slideDao = new SlideDao();
+    if(count($errors) == 0 ){
+        $presentationDao = new PresentationDao();
+        $presentationId = $presentationDao->save($heading, $categoryId, $uploadedImagePath);   
     
-    $fileContents = file_get_contents(__DIR__ . $uploadedImagePath);
-
-    $slides = json_decode($fileContents,true);
-
-    $order = 0;
-
-    foreach($slides as $slide){
-        $order++;
-        $slideHeading = $slide["heading"];
-        $slideTypeId = $slideTypeDao->getSlideTypeIdByLayout($slide["type"]);
-        $slideText = getValueByKeyInSlide("text", $slide);
-        $slideCodeblock = getValueByKeyInSlide("codeblock", $slide);
-        $slidePhoto = getValueByKeyInSlide("photo", $slide);
-        $slideList = json_encode(getValueByKeyInSlide("list", $slide));  
+        $slideTypeDao = new SlideTypeDao();
+        $slideDao = new SlideDao();
         
-        $slideDao->save($presentationId, $slideHeading, $slideText, $slideList, $slideCodeblock, $slidePhoto, $order, $slideTypeId);
+        $fileContents = file_get_contents(__DIR__ . $uploadedImagePath);
+    
+        $slides = json_decode($fileContents,true);
+    
+        $order = 0;
+    
+        foreach($slides as $slide){
+            $order++;
+            $slideHeading = $slide["heading"];
+            $slideTypeId = $slideTypeDao->getSlideTypeIdByLayout($slide["type"]);
+            $slideText = getValueByKeyInSlide("text", $slide);
+            $slideCodeblock = getValueByKeyInSlide("codeblock", $slide);
+            $slidePhoto = getValueByKeyInSlide("photo", $slide);
+            $slideList = json_encode(getValueByKeyInSlide("list", $slide));  
+            
+            $slideDao->save($presentationId, $slideHeading, $slideText, $slideList, $slideCodeblock, $slidePhoto, $order, $slideTypeId);
+        }
+    
+        header("Location:../views/presentation-with-slides.php?id=".$presentationId);
+        exit();
+    } else {
+        echo("<div class=\"container\"> <p>");
+        echo("! Warning ! Възникнаха грешки при запазването на информацията <br>");
+    	echo("Грешки: <br>");
+    
+    	foreach($errors as $err) {
+    		echo($err."<br>");
+		}
+		
+        echo("<a href=\"../views/create-presentation.php\">Опитайте отново</a>");
+        echo("</p></div>");
     }
-
-    header("Location:../views/presentation-with-slides.php?id=".$presentationId);
-    exit();
 }
+?> 
+</body>
+</html>
+
