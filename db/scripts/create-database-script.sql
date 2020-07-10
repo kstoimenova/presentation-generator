afda4a39fc1bc0127c39ab6d8cf3aa829ef99f46
mvardzhieva <maria9809@abv.bg>
2020-07-10 22:53:57 +0300

    Changed text area type to longtext and added 3 new presentations

--- db/scripts/create-database-script.sql
+++ db/scripts/create-database-script.sql
@@ -12,7 +12,7 @@ CREATE TABLE `slides` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `presentation_id` int,
   `heading` varchar(255),
-  `text_area` text DEFAULT NULL,
+  `text_area` longtext DEFAULT NULL,
   `list_json` json DEFAULT NULL,
   `codeblock` text DEFAULT NULL,
   `photo` varchar(255) DEFAULT NULL,
