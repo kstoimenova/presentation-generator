9c7236d8b6137237b3d10813ec1767e901d0518d
Katrin Stoimenova <katrinns123@gmail.com>
2020-07-11 22:15:19 +0300

    Add icon for edit

--- views/presentation-with-slides.php
+++ views/presentation-with-slides.php
@@ -29,6 +29,7 @@
     <link rel="stylesheet" href="../css/presentation-with-slides.css" />
     <title>Presentation</title>
     <link rel="shortcut icon" href="static/images/favicon-32x32.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
 
